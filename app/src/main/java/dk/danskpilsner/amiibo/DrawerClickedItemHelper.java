484bc91700c2ba9472b71304ee5ce13118642e7f
Lasse <lasse96-06@hotmail.com>
2019-04-10 11:57:43 +0200

    every end point is made

--- app/src/main/java/dk/danskpilsner/amiibo/DrawerClickedItemHelper.java
+++ app/src/main/java/dk/danskpilsner/amiibo/DrawerClickedItemHelper.java
@@ -16,112 +16,142 @@ public class DrawerClickedItemHelper
         switch (menuItem.getItemId())
         {
             case R.id.nav_animal_crossing:
-                showAnimalCrossing(context);
+                makeNetWorkCallForAmiibos(context, "Animal Crossing");
                 break;
             case R.id.nav_bayonetta:
-                showBayonetta(context);
+                makeNetWorkCallForAmiibos(context, "Bayonetta");
                 break;
             case R.id.nav_boxboy:
-                showBoxBoy(context);
+                makeNetWorkCallForAmiibos(context, "BoxBoy");
                 break;
             case R.id.nav_chibi_robo:
-                showChibiRobo(context);
+                makeNetWorkCallForAmiibos(context, "Chibi Robo");
                 break;
             case R.id.nav_classic_nintendo:
-                showClassicNintendo(context);
+                makeNetWorkCallForAmiibos(context, "Classic Nintendo");
                 break;
             case R.id.nav_dark_souls:
-                showDarkSouls(context);
+                makeNetWorkCallForAmiibos(context, "Dark Souls");
                 break;
             case R.id.nav_diablo:
-                showDiablo(context);
+                makeNetWorkCallForAmiibos(context, "Diablo");
                 break;
             case R.id.nav_donkey_kong:
-                showDonkeyKong(context);
+                makeNetWorkCallForAmiibos(context, "Donkey Kong");
                 break;
             case R.id.nav_earthbound:
-                showEarthbound(context);
+                makeNetWorkCallForAmiibos(context, "Earthbound");
                 break;
             case R.id.nav_f_zero:
-                showFZero(context);
+                makeNetWorkCallForAmiibos(context, "F-zero");
                 break;
             case R.id.nav_final_fantasy:
-                showFinalFantasy(context);
+                makeNetWorkCallForAmiibos(context, "Final Fantasy");
                 break;
             case R.id.nav_fire_emblem:
-                showFireEmblem(context);
+                makeNetWorkCallForAmiibos(context, "Fire Emblem");
                 break;
             case R.id.nav_kellogs:
-                showKellogs(context);
+                makeNetWorkCallForAmiibos(context, "Kellogs");
                 break;
             case R.id.nav_kid_icarus:
-                showKidIcarus(context);
+                makeNetWorkCallForAmiibos(context, "Kid Icarus");
                 break;
             case R.id.nav_kirby:
-                showKirby(context);
+                makeNetWorkCallForAmiibos(context, "Kirby");
                 break;
             case R.id.nav_mario_sports_superstars:
-                showMarioSuperStars(context);
+                makeNetWorkCallForAmiibos(context, "Mario Sports Superstars");
                 break;
             case R.id.nav_megaman:
-                showMegaman(context);
+                makeNetWorkCallForAmiibos(context, "Megaman");
                 break;
             case R.id.nav_metroid:
-                showMetroid(context);
+                makeNetWorkCallForAmiibos(context, "Metroid");
                 break;
             case R.id.nav_Mii:
-                showMii(context);
+                makeNetWorkCallForAmiibos(context, "Mii");
                 break;
             case R.id.nav_monster_hunter:
-                showMonsterHunter(context);
+                makeNetWorkCallForAmiibos(context, "Monster Hunter");
                 break;
             case R.id.nav_pac_man:
-                showPacman(context);
+                makeNetWorkCallForAmiibos(context, "Pac-man");
                 break;
             case R.id.nav_pikmin:
-                showPikmin(context);
+                makeNetWorkCallForAmiibos(context, "Pikmin");
                 break;
             case R.id.nav_pokemon:
-                showPokemon(context);
+                makeNetWorkCallForAmiibos(context, "Pokemon");
                 break;
             case R.id.nav_punch_out:
-                showPunchOut(context);
+                makeNetWorkCallForAmiibos(context, "Punch Out");
                 break;
             case R.id.nav_shovel_knight:
-                showShovelKnight(context);
+                makeNetWorkCallForAmiibos(context, "Shovel Knight");
                 break;
             case R.id.nav_sonic:
-                showSonic(context);
+                makeNetWorkCallForAmiibos(context, "Sonic");
                 break;
             case R.id.nav_splatoon:
-                showSplatoon(context);
+                makeNetWorkCallForAmiibos(context, "Splatoon");
                 break;
             case R.id.nav_star_fox:
-                showStarFox(context);
+                makeNetWorkCallForAmiibos(context, "Star Fox");
                 break;
             case R.id.nav_street_fighter:
-                showStreetFighter(context);
+                makeNetWorkCallForAmiibos(context, "Street Fighter");
                 break;
             case R.id.nav_super_mario:
-                showSuperMario(context);
+                makeNetWorkCallForAmiibos(context, "Super Mario");
                 break;
             case R.id.nav_wii_fit:
-                showWiiFit(context);
+                makeNetWorkCallForAmiibos(context, "Wii Fit");
                 break;
             case R.id.nav_xenoblade:
-                showXenoblade(context);
+                makeNetWorkCallForAmiibos(context, "Xenoblade");
                 break;
             case R.id.nav_yoshi:
-                showYoshi(context);
+                makeNetWorkCallForAmiibos(context, "Yoshi's Woolly World");
                 break;
             case R.id.nav_zelda:
-                showZelda(context);
+                makeNetWorkCallForAmiibos(context, "Zelda");
                 break;
             default:
                 break;
         }
 
 
+    }
+
+    private static void makeNetWorkCallForAmiibos(AppCompatActivity context, String gameSeries)
+    {
+        Call<AmiiboList> call = AmiiboServiceProvider.getService().getAmiibosFromSeries(gameSeries);
+        call.enqueue(new Callback<AmiiboList>()
+        {
+            @Override
+            public void onResponse(Call<AmiiboList> call, Response<AmiiboList> response)
+            {
+                if (context.getSupportFragmentManager().findFragmentById(R.id.fragment_container) != null)
+                {
+                    // replace content of list
+                    replaceContentOfFragment(context, response);
+                }
+                else
+                {
+                    // create new fragment, and do the transaction
+                    addNewFragment(context, response);
+                }
+
+
+            }
+
+            @Override
+            public void onFailure(Call<AmiiboList> call, Throwable t)
+            {
+
+            }
+        });
     }
 
     private static void addNewFragment(AppCompatActivity context, Response<AmiiboList> response)
@@ -167,173 +197,5 @@ public class DrawerClickedItemHelper
 
             }
         });
-    }
-
-
-
-
-    private static void showBayonetta(AppCompatActivity context)
-    {
-
-    }
-
-    private static void showBoxBoy(AppCompatActivity context)
-    {
-
-    }
-
-    private static void showChibiRobo(AppCompatActivity context)
-    {
-
-    }
-
-    private static void showClassicNintendo(AppCompatActivity context)
-    {
-
-    }
-
-    private static void showDarkSouls(AppCompatActivity context)
-    {
-
-    }
-
-    private static void showDiablo(AppCompatActivity context)
-    {
-
-    }
-
-    private static void showDonkeyKong(AppCompatActivity context)
-    {
-
-    }
-
-    private static void showEarthbound(AppCompatActivity context)
-    {
-
-    }
-
-    private static void showFZero(AppCompatActivity context)
-    {
-
-    }
-
-    private static void showFinalFantasy(AppCompatActivity context)
-    {
-
-    }
-
-    private static void showFireEmblem(AppCompatActivity context)
-    {
-
-    }
-
-    private static void showKellogs(AppCompatActivity context)
-    {
-
-    }
-
-    private static void showKidIcarus(AppCompatActivity context)
-    {
-
-    }
-
-    private static void showKirby(AppCompatActivity context)
-    {
-
-    }
-
-    private static void showMarioSuperStars(AppCompatActivity context)
-    {
-
-    }
-
-    private static void showMegaman(AppCompatActivity context)
-    {
-
-    }
-
-    private static void showMetroid(AppCompatActivity context)
-    {
-
-    }
-
-    private static void showMii(AppCompatActivity context)
-    {
-
-    }
-
-    private static void showMonsterHunter(AppCompatActivity context)
-    {
-
-    }
-
-    private static void showPacman(AppCompatActivity context)
-    {
-
-    }
-
-    private static void showPikmin(AppCompatActivity context)
-    {
-
-    }
-
-    private static void showPokemon(AppCompatActivity context)
-    {
-
-    }
-
-    private static void showPunchOut(AppCompatActivity context)
-    {
-
-    }
-
-    private static void showShovelKnight(AppCompatActivity context)
-    {
-
-    }
-
-    private static void showSonic(AppCompatActivity context)
-    {
-
-    }
-
-    private static void showSplatoon(AppCompatActivity context)
-    {
-
-    }
-
-    private static void showStarFox(AppCompatActivity context)
-    {
-
-    }
-
-    private static void showStreetFighter(AppCompatActivity context)
-    {
-
-    }
-
-    private static void showSuperMario(AppCompatActivity context)
-    {
-
-    }
-
-    private static void showWiiFit(AppCompatActivity context)
-    {
-
-    }
-
-    private static void showXenoblade(AppCompatActivity context)
-    {
-
-    }
-
-    private static void showYoshi(AppCompatActivity context)
-    {
-
-    }
-
-    private static void showZelda(AppCompatActivity context)
-    {
-
     }
 }
